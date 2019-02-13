71db57cdd6a8f4f93f7ef94bf9dd1d630b3ec05a
erajoc7 <Justin.Leger@McKesson.com>
2019-02-13 13:05:56 -0600

    final solution

--- main.py
+++ main.py
@@ -5,3 +5,27 @@
 # that new vector
 # find the norm of the final vector
 # also find the dot product between the 2nd and 3rd vector
+import vector as v
+
+
+vectors = []
+with open('vectors.txt') as f:
+    for line in f:
+        strvec = line.strip().split(',')
+        intvec = [int(n) for n in strvec]
+        vectors.append(v.Vector(*intvec))
+
+
+dim = vectors[0].dimension
+vectors[0] = vectors[0].scale(3)
+vectors[-1] = vectors[-1].scale(3)
+
+sumvector = v.Vector(*[0 for n in range(dim)])
+
+for n in range(len(vectors)):
+    sumvector += vectors[n]
+
+print('The list of vectors is: ', vectors)
+print('The sum of all vectors is: ', sumvector)
+print('The norm of the sum of vectors is: ', sumvector.norm())
+print('The dot product of the 2nd and 3rd vectors is: ', vectors[1].dotprd(vectors[2]))
--- vector.py
+++ vector.py
@@ -6,3 +6,33 @@
 # norm or len
 # dot product
 # override __str__ and __repr__
+
+
+class Vector():
+
+    def __init__(self, *vec):
+        self.vec = vec
+        self.dimension = len(self.vec)
+
+    def __add__(self, v):
+        return Vector(*[self.vec[n] + v.vec[n] for n in range(self.dimension)])
+
+    def __len__(self):
+        return self.norm()
+
+    def __str__(self):
+        return str(self.vec)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def norm(self):
+        sqrs = [self.vec[n]**2 for n in range(self.dimension)]
+        norm = sum(sqrs)**(1 / 2)
+        return norm
+
+    def scale(self, scalefactor):
+        return Vector(*[self.vec[n] * scalefactor for n in range(self.dimension)])
+
+    def dotprd(self, v):
+        return sum([self.vec[n] * v.vec[n] for n in range(self.dimension)])
